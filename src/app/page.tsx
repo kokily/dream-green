e8a875b3865035243cec4bc6745897bcfde4044c
김현성 <kokily@gimhyeonseong-ui-MacBookPro.local>
2024-07-13 16:21:16 +0900

    Under Construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import styles from './test.module.scss';
+import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function HomePage() {
   return (
-    <div className={styles.button}>버튼 테스트</div>
-  )
+    <div className={styles.main}>
+      <div>
+        <Image
+          src="/under-construction.jpg"
+          width={500}
+          height={450}
+          alt="홈페이지 공사 중"
+        />
+      </div>
+      <h3>곧 오픈할 예정입니다</h3>
+      <h2>(주)꿈에그린환경</h2>
+    </div>
+  );
 }
